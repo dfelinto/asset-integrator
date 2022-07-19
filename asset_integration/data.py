40cf8e78b972ec2a0e98e671ce041a490e41daef
Dalai Felinto <dfelinto@gmail.com>
2022-07-19 23:54:08 +0200

    Update data

--- asset_integration/data.py
+++ asset_integration/data.py
@@ -155,13 +155,59 @@ library_mock = {
             'is_operator': True,
         }
     },
-    'Lonely Node': {
-        'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
-        'type': 'NODE_TREE',
-        'subtype': 'GEOMETRY_NODES',
-        'is_modifier': False,
-        'is_node': True,
-        'is_operator': True,
+    'Select': {
+        'Odd Hairs': {
+            'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
+            'type': 'NODE_TREE',
+            'subtype': 'GEOMETRY_NODES',
+            'is_modifier': False,
+            'is_node': False,
+            'is_operator': True,
+        },
+        'Even More Hairs': {
+            'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
+            'type': 'NODE_TREE',
+            'subtype': 'GEOMETRY_NODES',
+            'is_modifier': False,
+            'is_node': False,
+            'is_operator': True,
+        },
+    },
+    'Curves': {
+        'Make Braids': {
+            'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
+            'type': 'NODE_TREE',
+            'subtype': 'GEOMETRY_NODES',
+            'is_modifier': False,
+            'is_node': False,
+            'is_operator': True,
+        },
+        'Make Straight Hair': {
+            'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
+            'type': 'NODE_TREE',
+            'subtype': 'GEOMETRY_NODES',
+            'is_modifier': False,
+            'is_node': False,
+            'is_operator': True,
+        },
+    },
+    'View': {
+        'View More Hair': {
+            'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
+            'type': 'NODE_TREE',
+            'subtype': 'GEOMETRY_NODES',
+            'is_modifier': False,
+            'is_node': False,
+            'is_operator': True,
+        },
+        'View Less Hair': {
+            'filepath': "Geometry Nodes Utils/einar_hair_tools.blend",
+            'type': 'NODE_TREE',
+            'subtype': 'GEOMETRY_NODES',
+            'is_modifier': False,
+            'is_node': False,
+            'is_operator': True,
+        },
     },
     'New Category':
     {
