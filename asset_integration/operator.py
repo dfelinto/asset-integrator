bdea56e2d51aa6a7e4d969c3118a3577396003f7
Dalai Felinto <dalai@blender.org>
2022-07-20 11:51:52 +0200

    Revert "Better operator description"
    Added two things in the same commit
    
    This reverts commit e2b4536c9ad31a5dd3db01d5c2362f5eacd981f2.

--- asset_integration/operator.py
+++ asset_integration/operator.py
@@ -27,32 +27,32 @@ class NODES_OT_add_asset_node(bpy.types.Operator):
     id_name: bpy.props.StringProperty(name="ID Name")
 
     def execute(self, context):
-        node_group = bpy.data.node_groups.get(self.id_name)
+        if self.id_name not in bpy.data.node_groups:
+            file_path = self.filepath
+            inner_path = 'NodeTree'
+            asset_name = self.id_name
 
-        # Only re-use data-block if it comes from the correct library
-        if node_group and node_group.library:
-            if node_group.library.filepath != self.filepath:
-                node_group = None
+            bpy.ops.wm.append(
+                filepath=os.path.join(file_path, inner_path, asset_name),
+                directory=os.path.join(file_path, inner_path),
+                filename=asset_name
+            )
+            bpy.data.node_groups.get(self.id_name).asset_clear()
 
-        if not node_group:
-            node_group = get_asset_datablock_node_groups(
-                self.filepath, self.id_name)
-
-        else:
-            bpy.ops.node.add_node(
-                type="GeometryNodeGroup",
-                use_transform=True,
-                settings=[
-                    {
-                        "name": "node_tree",
-                        "value": "bpy.data.node_groups['{}']".format(node_group.name),
-                    }])
+        bpy.ops.node.add_node(
+            type="GeometryNodeGroup",
+            use_transform=True,
+            settings=[
+                {
+                    "name": "node_tree",
+                    "value": "bpy.data.node_groups['{}']".format(self.id_name),
+                }])
 
         return {'FINISHED'}
 
 
 class NODES_OT_asset_operator(bpy.types.Operator):
-    """Run node group as operator"""
+    """Run node group tool"""
     bl_idname = "nodes.add_asset_operator"
     bl_label = "Add Asset Operator"
 
